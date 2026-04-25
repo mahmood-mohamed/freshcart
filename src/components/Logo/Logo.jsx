7d63a667d73a257a31b33a4198d82afdb5f97a8e
m2002d <firstyear265@gmail.com>
2026-04-25 15:56:59 +0300

    feat: update style of logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Logo() {
   return (
-    <div className="font-bold text-2xl pl-1">
-      <i className="fa-solid fa-cart-shopping text-green-500 pe-1"></i>
-      FreshCart
-    </div>
-
-
+    <Link 
+      to="/" 
+      className="font-bold text-2xl cursor-pointer select-none flex items-center"
+      aria-label="FreshCart home"
+    >
+      <i className="fa-solid fa-cart-shopping text-green-500 pe-1" aria-hidden="true"></i>
+      Fresh<span className='text-green-500'>Cart</span>
+    </Link>
   )
 }
