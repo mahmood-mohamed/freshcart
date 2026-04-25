dcf622d2c3b4ab4e46878566055afa86f0a01b9b
m2002d <firstyear265@gmail.com>
2026-04-25 14:15:46 +0300

    feat: add setting to ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,18 @@ function App() {
         <CartItemsContext>
           <WishlistProvider>
             <RouterProvider router={router}/>
-            <ToastContainer />
+            <ToastContainer 
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
             <ReactQueryDevtools/>
           </WishlistProvider>
         </CartItemsContext>
